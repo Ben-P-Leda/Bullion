f40aa0abc1b0d73ccc769b9768b6bcf9403534ce
Ben-P-Leda <benp@ledaentertainment.co.uk>
2016-05-05 13:27:11 +0100

    camera zoom based on avatar proximity

--- Project/Bullion/Assets/Scripts/PlayerInput.cs
+++ Project/Bullion/Assets/Scripts/PlayerInput.cs
@@ -4,13 +4,15 @@ namespace Assets.Scripts
 {
     public class PlayerInput : MonoBehaviour
     {
+        public string AxisPrefix;
+
         public float Horizontal { get; private set; }
         public float Vertical { get; private set; }
 
         private void FixedUpdate()
         {
-            Horizontal = Input.GetAxis("Horizontal");
-            Vertical = Input.GetAxis("Vertical");
+            Horizontal = Input.GetAxis(AxisPrefix + "-Horizontal");
+            Vertical = Input.GetAxis(AxisPrefix + "-Vertical");
         }
     }
 }
