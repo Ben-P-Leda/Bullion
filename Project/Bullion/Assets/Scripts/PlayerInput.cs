8399f72e074cf3c63e370f12718b784c344a7e32
Ben-P-Leda <benp@ledaentertainment.co.uk>
2016-05-05 05:51:10 +0100

    Initial setup with placeholders

---
+++ Project/Bullion/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+
+        private void FixedUpdate()
+        {
+            Horizontal = Input.GetAxis("Horizontal");
+            Vertical = Input.GetAxis("Vertical");
+        }
+    }
+}
---
+++ Project/Bullion/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Transform _transform;
+        private Rigidbody _rigidBody;
+        private PlayerInput _input;
+        private Terrain _terrain;
+
+        private void Start()
+        {
+            _transform = transform;
+            _rigidBody = GetComponent<Rigidbody>();
+            _input = GetComponent<PlayerInput>();
+            _terrain = Terrain.activeTerrain;
+        }
+
+        private void Update()
+        {
+            Vector3 inputVelocity = new Vector3(_input.Horizontal, 0.0f, _input.Vertical).normalized * Movement_Speed;
+
+            if (inputVelocity != Vector3.zero)
+            {
+                _rigidBody.velocity = new Vector3(inputVelocity.x, _rigidBody.velocity.y, inputVelocity.z);
+                _transform.LookAt(_transform.position + inputVelocity);
+            }
+
+            float groundHeight = _terrain.SampleHeight(_transform.position);
+            _transform.position = new Vector3(_transform.position.x, Mathf.Max(_transform.position.y, groundHeight), _transform.position.z);
+        }
+
+        private const float Movement_Speed = 5.0f;
+    }
+}
