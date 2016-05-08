c6059222b446ff52061b816a18bcc22e155cbc93
Ben-P-Leda <benp@ledaentertainment.co.uk>
2016-05-08 06:33:40 +0100

    Water drag slows movement

--- Project/Bullion/Assets/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Project/Bullion/Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -10,6 +10,10 @@ namespace Assets.Scripts.Gameplay.Player
         private PlayerInput _input;
         private Terrain _terrain;
 
+        private float _seaEntryHeight;
+        private float _swimHeight;
+        private float _wadeHeightRange;
+
         public CharacterConfiguration Configuration { private get; set; }
 
         private void Start()
@@ -18,11 +22,16 @@ namespace Assets.Scripts.Gameplay.Player
             _rigidBody = GetComponent<Rigidbody>();
             _input = GetComponent<PlayerInput>();
             _terrain = Terrain.activeTerrain;
+
+            _seaEntryHeight = GetComponent<CapsuleCollider>().height * Sea_Entry_Height_Modifier;
+            _swimHeight = GetComponent<CapsuleCollider>().height * Swim_Height_Modifier;
+            _wadeHeightRange = _seaEntryHeight - _swimHeight;
         }
 
         private void Update()
         {
-            Vector3 inputVelocity = new Vector3(_input.Horizontal, 0.0f, _input.Vertical).normalized * Configuration.MovementSpeed;
+            float speed = GetMovementSpeed();
+            Vector3 inputVelocity = new Vector3(_input.Horizontal, 0.0f, _input.Vertical).normalized * speed;
 
             if (inputVelocity != Vector3.zero)
             {
@@ -33,5 +42,23 @@ namespace Assets.Scripts.Gameplay.Player
             float groundHeight = _terrain.SampleHeight(_transform.position);
             _transform.position = new Vector3(_transform.position.x, Mathf.Max(_transform.position.y, groundHeight), _transform.position.z);
         }
+
+
+        private float GetMovementSpeed()
+        {
+            float speed = Configuration.MovementSpeed;
+
+            if (_transform.position.y < _seaEntryHeight)
+            {
+                float depthOffset = Mathf.Clamp((_seaEntryHeight - _transform.position.y) / _wadeHeightRange, 0.0f, 1.0f);
+                speed *=  (1.0f - (Swim_Speed_Modifier * depthOffset));
+            }
+
+            return speed;
+        }
+
+        private const float Sea_Entry_Height_Modifier = 0.5f;
+        private const float Swim_Height_Modifier = 0.05f;
+        private const float Swim_Speed_Modifier = 0.4f;
     }
 }
