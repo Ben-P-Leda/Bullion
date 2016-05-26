cee470fb107a46191aeef988f29680c83b8d1883
Ben-P-Leda <benp@ledaentertainment.co.uk>
2016-05-26 18:30:24 +0100

    start treasure placement grid setup

--- Project/Bullion/Assets/Scripts/Gameplay/Treasure/ChestFactory.cs
+++ Project/Bullion/Assets/Scripts/Gameplay/Treasure/ChestFactory.cs
@@ -4,23 +4,44 @@ namespace Assets.Scripts.Gameplay.Treasure
 {
     public class ChestFactory : MonoBehaviour
     {
-        private Terrain _terrain;
+        private ChestPlacementGrid _placementGrid;
 
         public GameObject ChestPrefab;
         public float ChestHitPoints;
 
         private void Start()
         {
-            _terrain = Terrain.activeTerrain;
+            _placementGrid = new ChestPlacementGrid(Terrain.activeTerrain, 1.0f, 2);
 
-            Vector3 xzPosition = new Vector3(22.0f, 0.0f, 20.0f);
-            Vector3 startPosition = new Vector3(xzPosition.x, _terrain.SampleHeight(xzPosition) + Hidden_Chest_Vertical_Offset, xzPosition.z);
-            GameObject chest = (GameObject)Instantiate(ChestPrefab);
-            chest.SetActive(true);
+            Transform[] fixedObstructions = GetFixedObstructions();
+            _placementGrid.BlockOutFixedObstructions(fixedObstructions, 2);
+            
 
-            chest.GetComponent<ChestEntry>().SecondsToLaunch = 10.0f;
-            chest.GetComponent<ChestEntry>().StartPosition = startPosition;
-            chest.GetComponent<ChestCollisions>().HitPoints = ChestHitPoints;
+            for (int x = 0; x<_placementGrid.Width; x++)
+            {
+                for (int z = 0; z<_placementGrid.Depth; z++)
+                {
+                    if (_placementGrid.GetCellState(x, z) == ChestPlacementGrid.CellState.Available)
+                    {
+                        GameObject box = (GameObject)Instantiate(ChestPrefab);
+                        box.transform.position = new Vector3(x + 0.5f, 3.0f, z + 0.5f);
+                    }
+                }
+            }
+        }
+
+        private Transform[] GetFixedObstructions()
+        {
+            GameObject obstructionContainer = GameObject.Find("Obstructions");
+            int obstructionCount = obstructionContainer != null ? obstructionContainer.transform.childCount : 0;
+            Transform[] obstructions = new Transform[obstructionCount];
+
+            for (int i = 0; i < obstructionCount; i++)
+            {
+                obstructions[i] = obstructionContainer.transform.GetChild(i);
+            }
+
+            return obstructions;
         }
 
         private const float Hidden_Chest_Vertical_Offset = -2.0f;
---
+++ Project/Bullion/Assets/Scripts/Gameplay/Treasure/ChestPlacementGrid.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay.Treasure
+{
+    public class ChestPlacementGrid
+    {
+        private CellState[][] _placementGrid;
+        private float _cellSize;
+
+        public int Width { get; private set; }
+        public int Depth { get; private set; }
+
+        public ChestPlacementGrid(Terrain terrain, float cellSize, int neighboursToExclude)
+        {
+            Width = (int)(terrain.terrainData.size.x / cellSize);
+            Depth = (int)(terrain.terrainData.size.z / cellSize);
+
+            _cellSize = cellSize;
+
+            CreateGridContainer(Width, Depth);
+            MarkCellsUnavailableByGroundHeight(Width, Depth, terrain, cellSize, neighboursToExclude);
+        }
+
+        private void CreateGridContainer(int width, int depth)
+        {
+            _placementGrid = new CellState[width][];
+            for (int x = 0; x < width; x++)
+            {
+                _placementGrid[x] = new CellState[depth];
+                for (int z = 0; z < depth; z++)
+                {
+                    _placementGrid[x][z] = CellState.Unassigned;
+                }
+            }
+        }
+
+        private void MarkCellsUnavailableByGroundHeight(int width, int depth, Terrain terrain, float cellSize, int neightboursToExclude)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Vector3 terrainPosition = new Vector3((x + (cellSize * 0.5f)) * cellSize, 0.0f, (z + (cellSize * 0.5f)) * cellSize);
+
+                    if (Terrain.activeTerrain.SampleHeight(terrainPosition) >= Assignable_Cell_Minimum_Height)
+                    {
+                        SetGridCellStateIfNotPermanentlyUnavailable(x, z, CellState.Available);
+                    }
+                    else
+                    {
+                        BlockOutCellAndNeighbours(x, z, neightboursToExclude);
+                    }
+                }
+            }
+        }
+
+        private void SetGridCellStateIfNotPermanentlyUnavailable(int gridX, int gridZ, CellState gridCellState)
+        {
+            if (_placementGrid[gridX][gridZ] != CellState.PermanentlyUnavailable)
+            {
+                _placementGrid[gridX][gridZ] = gridCellState;
+            }
+        }
+
+        private void BlockOutCellAndNeighbours(int centerGridX, int centerGridZ, int neighboursToExclude)
+        {
+            for (int neighbourX = -neighboursToExclude; neighbourX <= neighboursToExclude; neighbourX++)
+            {
+                for (int neighbourZ = -neighboursToExclude; neighbourZ <= neighboursToExclude; neighbourZ++)
+                {
+                    int gridX = (int)Mathf.Clamp(centerGridX + neighbourX, 0, _placementGrid.Length - 1);
+                    int gridZ = (int)Mathf.Clamp(centerGridZ + neighbourZ, 0, _placementGrid[gridX].Length - 1);
+                    _placementGrid[gridX][gridZ] = CellState.PermanentlyUnavailable;
+                }
+            }
+        }
+
+        public void BlockOutFixedObstructions(Transform[] obstructions, int margin)
+        {
+            for (int i = 0; i < obstructions.Length; i++)
+            {
+                int gridX = (int)(obstructions[i].position.x / _cellSize);
+                int gridZ = (int)(obstructions[i].position.z / _cellSize);
+
+                BlockOutCellAndNeighbours(gridX, gridZ, margin);
+            }
+        }
+
+        public CellState GetCellState(int x, int z)
+        {
+            return _placementGrid[x][z];
+        }
+
+        public enum CellState
+        {
+            Unassigned = 0,
+            TemporarilyUnavailable = 1,
+            PermanentlyUnavailable = 2,
+            Available = 3
+        }
+
+        private const float Assignable_Cell_Minimum_Height = 1.2f;
+        private const int Neighbours_To_Exclude = 2;
+    }
+}
